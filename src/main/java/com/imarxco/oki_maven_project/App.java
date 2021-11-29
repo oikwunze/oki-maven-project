8002632fab621761f34086cda1907f098c65364e
Okechukwu Ikwunze <okechukwuikwunze@yahoo.com>
2021-11-28 18:06:38 -0600

    added initial commit

---
+++ src/main/java/com/imarxco/oki_maven_project/App.java
@@ -0,0 +1,13 @@
+package com.imarxco.oki_maven_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Okechukwu ikwunze!" );
+    }
+}
